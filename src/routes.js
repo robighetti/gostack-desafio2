daf06eb9542aea96eef8fb0f14c7f5de133789cd
Rodrigo Bighetti <rodrigo.bighetti@i-value.com.br>
2020-04-17 12:08:35 -0300

    fim do desafio

--- src/routes.js
+++ src/routes.js
@@ -1,27 +1,69 @@
-const express = require('express')
+const { Router } = require('express');
+const { uuid } = require("uuidv4");
 
-const routes = express.Router();
+const routes = Router();
 
 const repositories = [];
 
 routes.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
+
+
 });
 
 routes.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs, likes = 0 } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 routes.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  repositories.map(repo => {
+    if (repo.id === id) {
+      repo.title = title,
+      repo.url = url,
+      repo.techs = techs
+    }
+  })
+
+  return response.json(repositories);
 });
 
 routes.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;  
+
+  repositories.map((repo, index) => {
+    if (repo.id === id) {
+      repositories.splice(index)
+    }
+  })
+
+  return response.json(repositories);
 });
 
 routes.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;  
+
+  repositories.map(repo => {
+    if (repo.id === id) {
+      repo.likes ++;
+    }
+  })
+
+  return response.json(repositories);
 });
 
 module.exports = routes;
